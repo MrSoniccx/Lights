e734d5d7b7d67ee630169cf428d6f056286aa576
MrSoniccx <yahel_isordia@hotmail.com>
2022-05-17 08:38:45 -0500

    First phase snake done

--- Assets/BossSnake.cs
+++ Assets/BossSnake.cs
@@ -9,13 +9,15 @@ public class BossSnake : MonoBehaviour
     SpriteRenderer SnakeSr;
     Transform target;
     Animator animator;
+    public List<GameObject> pointsTP = new List<GameObject>();
+    int children;
 
 
     /*---------------------------
     Variables para Condiciones
     ---------------------------*/
     
-    private string animationHelper="";
+    private string animationHelper="Safe";
     private string state="";
     private float healthAmount;
     private float healthAmountMax;
@@ -28,16 +30,23 @@ public class BossSnake : MonoBehaviour
     /*---------------------------
     Variables para ataques especiales
     ---------------------------*/
+    private int randomNumber=0;
+    private float timer=0f;
+    private float boostTimerHelp=0f;
+    public float timerBetween=2f;
+    public float boostTimer=2.2f;
+    public float tpRespawn=2f;
+    public float transparent=2f;
+    private float transparentHelp=1f;
 
-    private float timerSpring=0f;
-    public float timerIntercalando;
-    private float boostTimer=0f;
 
 
 
 
     //Animaciones
     const string SNAKE_SPRING = "SnakeSpring";
+    const string MOVING_FOWARD = "MovingFoward";
+    
 
     // Start is called before the first frame update
     void Start()
@@ -49,6 +58,11 @@ public class BossSnake : MonoBehaviour
         animator = GetComponent<Animator>();
         target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
         ChangeAnimationState("Safe");
+        children = GameObject.FindGameObjectWithTag("TPBoss").transform.childCount;
+        for(int i=0; i<children;i++){
+        pointsTP[i] = GameObject.FindGameObjectWithTag("TPBoss").transform.GetChild(i).gameObject;
+        Debug.Log(pointsTP[i]);
+        }
     }
 
     void Update(){
@@ -61,7 +75,7 @@ public class BossSnake : MonoBehaviour
     void FixedUpdate()
     {
         //-----First Phase-----//
-        if(healthPercent>66 && healthPercent<=100){
+        if(healthPercent>0 && healthPercent<=100){
 
             /*-----------------------------
             Primera fase: Lista de ataques
@@ -72,7 +86,38 @@ public class BossSnake : MonoBehaviour
 
             -----------------------------*/
 
-            // if (Input.GetKey("space")){SnakeSr.color = new Color(1, 1, 1, 0.5f);}
+            
+            if(attack=="Special"){ 
+
+                if(randomNumber == 0)
+                {randomNumber= Random.Range(1,3);}
+
+                switch (randomNumber){
+                    case 1:
+                        Spring();
+                        LookPlayer(90f);
+                        break;
+                    case 2:
+                        Camuflaje();
+                        LookPlayer(90f);
+                        break;
+                }
+                
+                
+                
+            }
+            
+            if(attack==MOVING_FOWARD){
+                MoveTowardsPlayer(1f);
+                LookPlayer(90f);
+                timer+=1*Time.deltaTime;
+                if(timer >= timerBetween){timer=0;attack="Special";}
+
+
+                
+            }
+
+           
 
         }
 
@@ -112,21 +157,14 @@ public class BossSnake : MonoBehaviour
 
 
         
-        if(timerSpring >= timerIntercalando){
-            Spring();}
-        else{timerSpring += 1*Time.deltaTime;
-        MoveTowardsPlayer(1f);
-        }
-        Vector2 lookDir = target.position-transform.position;
-        float angle = Mathf.Atan2(lookDir.y,lookDir.x)* Mathf.Rad2Deg+90f;
-        SnakeRb.rotation = angle;
+        
         
 
 
     }
         
     void Spring(){
-        if(boostTimer==0){ChangeAnimationState(SNAKE_SPRING);}
+        if(boostTimerHelp==0){ChangeAnimationState(SNAKE_SPRING);}
         
         if(animationHelper=="Realize"){
         MoveTowardsPlayer(4f);
@@ -134,14 +172,50 @@ public class BossSnake : MonoBehaviour
         if(animationHelper=="EndedSpring"){
             ChangeAnimationState("Safe");
             MoveTowardsPlayer(1.8f);
-            boostTimer+=1*Time.deltaTime;
-            if(boostTimer>=2.2){
-                boostTimer=0;
-                timerSpring=0f;
-                animationHelper="";
+            boostTimerHelp+=1*Time.deltaTime;
+            if(boostTimerHelp>=boostTimer){
+                boostTimerHelp=0;
+                animationHelper="Safe";
+                randomNumber=0;
+                attack=MOVING_FOWARD;
             }
             }
+    }
+
+
+    void Camuflaje(){
+        if(animationHelper=="Safe"){
+            MoveTowardsPlayer(0.75f);
+        SnakeSr.color = new Color(1, 1, 1, transparentHelp);
+        transparentHelp-=transparent*Time.deltaTime;
+        if(transparentHelp<=0){transform.position = pointsTP[0].transform.position; animationHelper="Fuga";}
+        }
+        if(animationHelper=="Fuga")
+        {
+            transparentHelp += transparent*Time.deltaTime;
+            if(transparentHelp >= tpRespawn)
+            {
+                int i=Random.Range(1,children);
+                transform.position = pointsTP[i].transform.position;
+                animationHelper="Respawn";
+                transparentHelp=0f;
+            }
+        }
+        if(animationHelper=="Respawn"){
+            transparentHelp+=1*Time.deltaTime;
+            SnakeSr.color = new Color(1, 1, 1, transparentHelp);
+            if(transparentHelp>=1){
+                transparentHelp=1f;
+                animationHelper="Safe";
+                SnakeSr.color = new Color(1, 1, 1, transparentHelp);
+                randomNumber=0;
+                attack=MOVING_FOWARD;
+            }
+            MoveTowardsPlayer(0.75f);
+        }
+
         
+        LookPlayer(90f);
     }
 
     public void State(string imIn){
@@ -162,9 +236,17 @@ public class BossSnake : MonoBehaviour
         transform.position = Vector2.MoveTowards(transform.position, target.position, speed*Time.deltaTime*Multiplier);
     }
 
+    void LookPlayer(float deg){
+
+        Vector2 lookDir = target.position-transform.position;
+        float angle = Mathf.Atan2(lookDir.y,lookDir.x)* Mathf.Rad2Deg+deg;
+        SnakeRb.rotation = angle;
+    }
+
 
     
  }
+
 
 
 
