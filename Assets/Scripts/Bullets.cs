4a6bfb08f71cf4d5999627184f39c0939fbcea9d
EdgvrGM <edgar_gm6@hotmail.com>
2022-05-16 21:01:46 -0500

    avance

---
+++ Assets/Scripts/Bullets.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullets : MonoBehaviour
+{
+
+    private Vector2 moveDirection;
+    private float moveSpeed;
+
+    private void OnEnable()
+    {
+        Invoke("Destroy", 3f);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveSpeed = 5f;    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+
+          
+        Vector2 currentPosition = new Vector2(transform.position.x, transform.position.y);
+        Vector2 newPosition = currentPosition + moveSpeed * Time.deltaTime;
+
+        Debug.DrawLine(currentPosition, newPosition, Color.red);
+
+        RaycastHit2D[] hits = Physics2D.LinecastAll(currentPosition, newPosition);
+
+        foreach (RaycastHit2D hit in hits) {
+            GameObject other = hit.collider.gameObject;
+            if( other != this.gameObject){
+            //Debug.Log(hit.collider.gameObject);
+            GameObject light = Instantiate(lightPrefab, transform.position, Quaternion.identity);
+            soundMan.PlaySound("pHit");
+            Destroy(gameObject,0.0f);
+            }
+
+            if( other.tag == "Player"){
+                Vector2 temp = (transform.position-other.transform.position).normalized;
+
+                other.GetComponent<Health>().TakeDamage(temp);
+            }
+
+
+        transform.position = newPosition;
+     }
+    }
+
+    public void SetMoveDirections(Vector2 dir)
+    {
+        moveDirection = dir;
+    }
+
+    public void Destroy()
+    {
+        gameObject.SetActive(false);
+    }
+
+    private void OnDisable()
+    {
+        CancelInvoke();
+    }
+}
